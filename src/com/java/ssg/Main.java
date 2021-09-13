2fbddf911d89c253ae847f7b59c0250c27665658
yoonwoo <0421mk@naver.com>
2021-09-13 22:22:32 +0900

    게시물 작성기능 구현- 1

--- src/com/java/ssg/Main.java
+++ src/com/java/ssg/Main.java
@@ -6,16 +6,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("=== 프로그램 시작 ===");
+
 		Scanner sc = new Scanner(System.in);
+		int articleId = 0;
+
+		while (true) {
+			System.out.printf("명령어 입력 : ");
+			String command = sc.nextLine();
+
+			if (command.equals("article write")) {
+				
+				articleId++;
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				
+				System.out.printf("내용 : ");
+				String content = sc.nextLine();
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", articleId);
+				
+			} else if (command.equals("system exit")) {
+				System.out.println("system exit");
+				break;
+			} else {
+				System.out.println("존재하지 않는 명령어입니다.");
+			}
+		}
 		
-		System.out.printf("문자열 입력 : ");
-		String command = sc.nextLine();
-		
-		System.out.printf("정수 입력 : ");
-		int intCommand = sc.nextInt();
-		
-		System.out.printf("입력된 문자열 : %s\n", command);
-		System.out.printf("입력된 정수 : %d\n", intCommand);
+		sc.close();
+		System.out.println("=== 프로그램 종료 ===");
 
 	}
 
