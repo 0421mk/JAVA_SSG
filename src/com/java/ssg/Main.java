3d47a8c9bd17662a67c2bb673aa9817860ffa66c
yoonwoo <0421mk@naver.com>
2021-09-13 23:31:27 +0900

    게시물 상세페이지 기능 구현

--- src/com/java/ssg/Main.java
+++ src/com/java/ssg/Main.java
@@ -18,18 +18,18 @@ public class Main {
 			String command = sc.nextLine();
 
 			if (command.equals("article write")) {
-				
+
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
-				
+
 				System.out.printf("내용 : ");
 				String content = sc.nextLine();
-				
+
 				Article article = new Article(title, content);
 				articles.add(article);
-				
+
 				System.out.printf("%d번 글이 생성되었습니다.\n", article.id);
-				
+
 			} else if (command.equals("article list")) {
 
 				for (Article article : articles) {
@@ -38,7 +38,38 @@ public class Main {
 					System.out.printf("내용 : %s\n", article.content);
 					System.out.println("===========");
 				}
-			
+
+			} else if (command.startsWith("article detail")) {
+
+				command = command.substring("article detail".length()).trim();
+
+				if (isNumeric(command) == false) {
+					System.out.println("한칸 띄고 숫자만 입력해주세요.");
+					continue;
+				}
+
+				int commandNum = Integer.parseInt(command);
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+
+					if (article.id == commandNum) {
+						foundArticle = article;
+						break;
+					}
+
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", commandNum);
+					continue;
+				}
+
+				System.out.printf("글 ID : %d\n", foundArticle.id);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.content);
+				System.out.println("===========");
+
 			} else if (command.equals("system exit")) {
 				System.out.println("system exit");
 				break;
@@ -46,10 +77,16 @@ public class Main {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
 		}
-		
+
 		sc.close();
 		System.out.println("=== 프로그램 종료 ===");
 
+	}
+
+	public static boolean isNumeric(String str) {
+		boolean check = str.matches("-?\\d+");
+
+		return check;
 	}
 }
 
@@ -58,7 +95,7 @@ class Article {
 	int id;
 	String title;
 	String content;
-	
+
 	Article(String title, String content) {
 		indexId++;
 		id = indexId;
